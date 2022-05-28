48432c47c502e3f8297339e4bc31466e07911930
richhdev <79666708+richhdev@users.noreply.github.com>
2022-05-28 18:07:17 +1000

    add basic functionality to Navigation component

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,49 +1,68 @@
+import { useState } from "react";
 import Link from "next/link";
+import styles from "./Navigation.module.css";
 
 function Navighation() {
+  const [menuIsActive, setMenuIsActive] = useState(false);
   return (
-    <ul>
-      <li>
-        <Link href="/home">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/bio">
-          <a>Bio</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/videos">
-          <a>Videos</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/photos">
-          <a>Photos</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/dj">
-          <a>DJ</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/tours">
-          <a>Tours</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/blog">
-          <a>Blog</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/contact">
-          <a>Contact</a>
-        </Link>
-      </li>
-    </ul>
+    <>
+      <div
+        className={styles.menuIcon}
+        onClick={() => {
+          setMenuIsActive(!menuIsActive);
+        }}
+      >
+        {menuIsActive ? <>close</> : <>menu</>}
+      </div>
+
+      <ul
+        className={styles.list}
+        style={{
+          left: menuIsActive ? "0" : "100%",
+        }}
+      >
+        <li className={styles.listItem}>
+          <Link href="/home">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/bio">
+            <a>Bio</a>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/videos">
+            <a>Videos</a>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/photos">
+            <a>Photos</a>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/dj">
+            <a>DJ</a>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/tours">
+            <a>Tours</a>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
+      </ul>
+    </>
   );
 }
 
