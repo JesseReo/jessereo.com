675736a3c8992d5115248bcd7ff8fb6107a93ad6
richhdev <79666708+richhdev@users.noreply.github.com>
2022-05-28 23:05:36 +1000

    add fullWidth prop to Main component

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -2,7 +2,12 @@ import styles from "./Main.module.css";
 
 export default function Main(props) {
   return (
-    <div className={styles.main} {...props}>
+    <div
+      className={styles.main}
+      style={{
+        maxWidth: props.fullWidth ? "100%" : "inherit",
+      }}
+    >
       {props.children}
     </div>
   );
