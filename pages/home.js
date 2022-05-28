4dd501c9b21bef9b5ca6fbad60db2ff7baf6a9b7
richhdev <79666708+richhdev@users.noreply.github.com>
2022-05-28 19:44:30 +1000

    Update Home Page with shared components

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
-import Navigation from "../components/Navigation";
 import styles from "../styles/Home.module.css";
+import Navigation from "../components/Navigation";
+import PageContainer from "../components/PageContainer";
+import Logo from "../components/Logo";
+import Main from "../components/Main";
 
 export default function Home() {
   return (
@@ -11,25 +14,22 @@ export default function Home() {
           name="description"
           content="The home page of Jesse Real the honest hommus adonis"
         />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        ></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <div className={styles.pageContainer}>
+      <PageContainer>
         <Navigation />
 
-        <main className={styles.main}>
-          <div className={styles.logo}>LOGO</div>
-        </main>
+        <Logo />
 
-        <footer className={styles.footer}>your local weather</footer>
+        <Main />
+
+        <footer className={styles.footer}>Your local weather</footer>
 
         <video className={styles.video} autoPlay muted loop>
           <source src="/SampleVideo_1280x720_1mb.mp4" type="video/mp4" />
         </video>
-      </div>
+      </PageContainer>
     </>
   );
 }
