70ed53d215f191d3dffd0ce9189c987cddcd7989
richhdev <79666708+richhdev@users.noreply.github.com>
2022-05-28 18:51:41 +1000

    add wireframe to home page

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Navigation from "../components/Navigation";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,11 +11,25 @@ export default function Home() {
           name="description"
           content="The home page of Jesse Real the honest hommus adonis"
         />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
       </Head>
-      <Navigation />
-      <main>
-        <h1>Home</h1>
-      </main>
+
+      <div className={styles.pageContainer}>
+        <Navigation />
+
+        <main className={styles.main}>
+          <div className={styles.logo}>LOGO</div>
+        </main>
+
+        <footer className={styles.footer}>your local weather</footer>
+
+        <video className={styles.video} autoPlay muted loop>
+          <source src="/SampleVideo_1280x720_1mb.mp4" type="video/mp4" />
+        </video>
+      </div>
     </>
   );
 }
