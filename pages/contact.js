9907b78e501924ba48a35e6abb87c3e56c4b97f0
richhdev <79666708+richhdev@users.noreply.github.com>
2022-05-29 15:24:27 +1000

    add wireframe for contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
-import DocumentHead from "../components/DocumentHead";
-import Navigation from "../components/Navigation";
+import styles from "../styles/Contact.module.css";
+import Hero from "../components/Hero";
+import Main from "../components/Main";
 
 export default function Contact() {
   return (
@@ -9,9 +10,47 @@ export default function Contact() {
         <title>Contact - JESSE REAL</title>
         <meta name="description" content="Contact details for Jesse Real" />
       </Head>
-      <main>
-        <h1>Contact</h1>
-      </main>
+      <Hero
+        title="Contact"
+        backgroundImage="url(/loan-7AIDE8PrvA0-unsplash.jpg)"
+      />
+      <Main>
+        <p className={styles.p}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+          hendrerit, diam in suscipit pulvinar.
+        </p>
+
+        <p className={styles.p}>
+          Email:
+          <a href="mailto:jesserehaut@gmail.com">jesserehaut@gmail.com</a>
+        </p>
+
+        <div className={styles.formContainer}>
+          <h2>Send a message</h2>
+          <form action="https://formspree.io/f/{form_id}" method="post">
+            <label for="email" className={styles.label}>
+              Your Email
+            </label>
+            <input
+              name="Email"
+              id="email"
+              type="email"
+              className={styles.input}
+            />
+            <label for="message" className={styles.label}>
+              Your Message
+            </label>
+            <textarea
+              name="Message"
+              id="message"
+              className={styles.message}
+            ></textarea>
+            <button type="submit" className={styles.submit}>
+              Submit
+            </button>
+          </form>
+        </div>
+      </Main>
     </>
   );
 }
