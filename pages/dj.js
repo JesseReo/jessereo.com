6185ff14c4d0f85d9c3404fe703bf060b5ea609e
richhdev <79666708+richhdev@users.noreply.github.com>
2022-05-31 20:00:18 +1000

    fix lint issue

--- pages/dj.js
+++ pages/dj.js
@@ -34,7 +34,7 @@ export default function Dj() {
             width="100%"
             height="300"
             scrolling="no"
-            frameborder="no"
+            frameBorder="no"
             allow="autoplay"
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1269291343&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
           />
