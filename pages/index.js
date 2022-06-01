6d694840cfa1e96f640997eac2c87410faeb8350
richhdev <79666708+richhdev@users.noreply.github.com>
2022-06-02 01:41:44 +1000

    try serve static file

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Hero backgroundImage="//Jesse+RUGS-3.jpg" />
+      <Hero backgroundImage="/Jesse+RUGS-3.jpg" />
       <Main />
       <footer className={styles.footer}>
         <Text>Your local weather</Text>
