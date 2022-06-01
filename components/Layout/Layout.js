1ed55e8c6c9835147d341bd850cb084b3f861834
richhdev <79666708+richhdev@users.noreply.github.com>
2022-06-02 00:11:58 +1000

    add logo and layout

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,12 +1,20 @@
-import Navigation from "../Navigation";
+import Link from "next/link";
+import styles from "./Layout.module.css";
 import PageContainer from "../PageContainer";
+import Navigation from "../Navigation";
 import Logo from "../Logo";
 
 export default function Layout({ children }) {
   return (
     <PageContainer>
       <Navigation />
-      <Logo />
+      <div className={styles.logoContainer}>
+        <Link href="/">
+          <a className={styles.a}>
+            <Logo />
+          </a>
+        </Link>
+      </div>
       {children}
     </PageContainer>
   );
