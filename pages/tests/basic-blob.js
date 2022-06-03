a95e0cc67b070e26807eca87d2ce75503b01ea0a
richhdev <79666708+richhdev@users.noreply.github.com>
2022-06-03 17:40:23 +1000

    fix build issues

--- pages/tests/basic-blob.js
+++ pages/tests/basic-blob.js
@@ -1,19 +1,19 @@
-import Head from "next/head";
-import styles from "../../styles/BasicBlob.module.css";
-import BasicBlob from "../../components/BasicBlob";
+// import Head from "next/head";
+// import styles from "../../styles/BasicBlob.module.css";
+// import BasicBlob from "../../components/BasicBlob";
 
-export default function Home() {
-  return (
-    <div className={styles.pageContainer}>
-      <Head>
-        <title>JESSE REAL LIVES</title>
-        <meta name="description" content="Powered by ✌️ hommus" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+// export default function Home() {
+//   return (
+//     <div className={styles.pageContainer}>
+//       <Head>
+//         <title>JESSE REAL LIVES</title>
+//         <meta name="description" content="Powered by ✌️ hommus" />
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
 
-      <div className={styles.blobContainer}>
-        <BasicBlob />
-      </div>
-    </div>
-  );
-}
+//       <div className={styles.blobContainer}>
+//         <BasicBlob />
+//       </div>
+//     </div>
+//   );
+// }
--- pages/tests/video-blob.js
+++ pages/tests/video-blob.js
@@ -1,19 +1,19 @@
-import Head from "next/head";
-import styles from "../../styles/VideoBlob.module.css";
-import VideoBlob from "../../components/VideoBlob";
+// import Head from "next/head";
+// import styles from "../../styles/VideoBlob.module.css";
+// import VideoBlob from "../../components/VideoBlob";
 
-export default function Home() {
-  return (
-    <div className={styles.pageContainer}>
-      <Head>
-        <title>JESSE REAL LIVES</title>
-        <meta name="description" content="Powered by ✌️ hommus" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+// export default function Home() {
+//   return (
+//     <div className={styles.pageContainer}>
+//       <Head>
+//         <title>JESSE REAL LIVES</title>
+//         <meta name="description" content="Powered by ✌️ hommus" />
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
 
-      <div className={styles.blobContainer}>
-        <VideoBlob />
-      </div>
-    </div>
-  );
-}
+//       <div className={styles.blobContainer}>
+//         <VideoBlob />
+//       </div>
+//     </div>
+//   );
+// }
