d89fcb6e7090a4fe9a8482f3e17d58ffe56f22a3
richhdev <79666708+richhdev@users.noreply.github.com>
2023-01-20 02:16:18 +1100

    add scroll behavior polyfill

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
-import "scroll-behavior-polyfill";
+import { useEffect } from "react";
+import smoothscroll from "smoothscroll-polyfill";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    smoothscroll.polyfill();
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
