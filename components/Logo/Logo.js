66f65c33ad48da786cca080280cf81ef5469f055
richhdev <79666708+richhdev@users.noreply.github.com>
2023-04-27 23:46:45 +1000

    add 3d spin to logo

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -8,7 +8,11 @@ export default function Logo() {
       <Link href="/">
         <a className={styles.a} aria-label="logo home">
           <div className={styles.logo}>
-            <LogoImg alt="logo" />
+            <LogoImg alt="logo" className={`${styles.svg} ${styles.svg2}`} />
+            <LogoImg alt="logo" className={`${styles.svg} ${styles.svg1}`} />
+            <LogoImg alt="logo" className={`${styles.svg} ${styles.svg3}`} />
+            <LogoImg alt="logo" className={`${styles.svg} ${styles.svg4}`} />
+            <LogoImg alt="logo" className={`${styles.svg} ${styles.svg5}`} />
           </div>
         </a>
       </Link>
