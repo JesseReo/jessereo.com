765aedd6d977e986f908286e89060d08fe56e755
richhdev <79666708+richhdev@users.noreply.github.com>
2023-05-17 16:39:14 +1000

    outline different sections on the homepage

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,10 @@ import TheDjSvg from "../images/the-dj.svg";
 import TheTravellerSvg from "../images/the-traveller.svg";
 import WakeUpSvg from "../images/wake-the-funk-up.svg";
 import Weather from "../components/Weather";
+import Main from "../components/Main";
+import Stack from "../components/Stack";
+import Heading2 from "../components/Heading2";
+import Text from "../components/Text";
 
 export default function Home() {
   const playIconRef = useRef();
@@ -66,6 +70,56 @@ export default function Home() {
           </svg>
         </div>
       </div>
+
+      <Main>
+        <Stack>
+          <Heading2>Jesse REO Touring / session musician</Heading2>
+          <Text>
+            Most recently Jesse brings his soulful energy touring worldwide with
+            Tash Sultana across the US, Canada, Europe, New Zealand, and
+            Australia.
+          </Text>
+          <hr />
+
+          <Heading2>study / cuba 2018</Heading2>
+          <Text>
+            Jesse took one-on-one lessons with Cuban multi instrumentalist Redy
+            Cobas, learning Bembe/Merengue/Rumba/Son/Songo rhythms while jamming
+            together in a hot, sweaty room in Havana.
+          </Text>
+          <hr />
+
+          <Heading2>Jakbuki / Perspective Records - 2016 </Heading2>
+          <Text>
+            In 2016, Jakubi Signs major recored label with Perspective records.
+            <br />
+            spent six months working intensely with Jimmy Jam &amp; Terry Lewis,
+            spending 5-6 days a week in the studio, working one-on-one, jamming
+            together into the early hours of the morning, writing songs and
+            eating every meal together.
+          </Text>
+          <hr />
+
+          <Heading2>Jakbuki / us tours - 2012</Heading2>
+          <Text>
+            Jesse is a founding member of Jakubi and signs with major record
+            label deal with Epic Records. <br />
+            Scouted by LA Reid, Jakubi tours the us four times selling out
+            300-500 cap rooms from New York City to Los Angeles. <br />
+            In between tours, writing and working with Macy Gray and Scott
+            Storch (The Roots/Dr. Dre/ 50 Cent/Beyonce/Nas) and supported
+            legendary artists such as Sublime, Fishbone, The Suffers.
+          </Text>
+          <hr />
+
+          <Heading2>Early life 2 -1988 - 2014</Heading2>
+          <Text>
+            He received his first drum lesson at the age of seventeen, when he
+            began private sessions with The Cat Empire&apos;s Will Hull-Brown.
+          </Text>
+          <hr />
+        </Stack>
+      </Main>
 
       <BigLink
         href="/bio"
