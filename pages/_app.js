0da6ea0e64f3d201257f0c1668a88ca904df33ee
richhdev <79666708+richhdev@users.noreply.github.com>
2023-05-24 20:04:43 +1000

    💄 update default font and text styles

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import { useEffect } from "react";
 import smoothscroll from "smoothscroll-polyfill";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import "@fontsource/source-sans-pro/400.css";
+import "@fontsource/source-sans-pro/600.css";
+import "@fontsource/source-sans-pro/700.css";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
