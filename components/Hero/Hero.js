d46f260ea0e8ffcbf223fff3b890c50bf1fd2064
richhdev <79666708+richhdev@users.noreply.github.com>
2023-05-31 11:23:16 +1000

    💄 update cursor to down arrow over hero

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./Hero.module.css";
 import ArrowDown from "../../images/arrow.svg";
+import arrowDown from "../../images/arrow.png";
 
 export default function Hero(props) {
   return (
@@ -11,6 +12,9 @@ export default function Hero(props) {
           document
             .querySelector(".nextSection")
             .scrollIntoView({ behavior: "smooth" });
+        }}
+        style={{
+          cursor: `url('${arrowDown.src}'), auto`,
         }}
       >
         <div className={styles.content}>
