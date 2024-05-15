2fc71d4d77cd0c9b205a2bb7e8884b6015ab7661
richhdev <79666708+richhdev@users.noreply.github.com>
2024-05-15 18:48:17 +1000

    revert

--- pages/videos.js
+++ pages/videos.js
@@ -16,7 +16,6 @@ import Pagination from "../components/Pagination";
 import Footer from "../components/Footer";
 // import Spacer from "../components/Spacer";
 import Seperator from "../components/Seperator";
-import HomeHero from "../components/HomeHero";
 
 export default function Videos() {
   return (
@@ -27,7 +26,7 @@ export default function Videos() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <HomeHero />
+      <Hero title="VideOS" titleImage={<VideoSvg />} backgroundImage={img} />
 
       <Main>
         <div className={styles.items}>
